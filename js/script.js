bdca7b254fa03dbc948e53b9b0748838b5f93bf6
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-12 16:59:46 +0000

    all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara Tyman All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // define numbers
+  const firstNumber = parseFloat(document.getElementById("first-number").value)
+  const secondNumber = parseFloat(document.getElementById("second-number").value)
+  let dividend = firstNumber
+  let divisor = secondNumber
+  let remainder = 0
+  // divide
+  let numberOfLoops = 0
+  let answer = 0
+  while (true) {
+    if (dividend < divisor) {
+      remainder = dividend
+      break
+    }
+    dividend = dividend - divisor
+    numberOfLoops++
+  }
+  answer = numberOfLoops
+  // print answer
+  document.getElementById("answer").innerHTML = "The answer is: " + answer + " R " + remainder
+}
